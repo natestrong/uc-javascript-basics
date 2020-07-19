64c42e9d5cd880f2a5fd687debe16ac1a0aa93a1
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-18 17:08:01 -0700

    refactor app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,3 +2,19 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
+
+const condition = true;
+
+if (condition) {
+    console.log('yes');
+}
+
+const num = 99..toFixed();
+
+console.log(num);
+
+function makeCarPrice() {
+    console.log(arguments)
+}
+
+makeCarPrice(1, 2, 3, 45)

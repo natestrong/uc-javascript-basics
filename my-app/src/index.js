182619c0908feba90a3ba43d1d2bda4008917bd7
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-19 13:16:59 -0700

    refactor app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -29,21 +29,38 @@ const drink = {
 
 console.log(drink.getDrinkDetails());
 
-console.log(drink["100"])
+console.log(drink["100"]);
 
 function f(first, ...args) {
-    console.log(first, args)
+    console.log(first, args);
 }
 
-f('Hi', 'Hello', 'Cool', 'Awesome', 100)
+f('Hi', 'Hello', 'Cool', 'Awesome', 100);
 
 
 const cat = {
     name: 'Oliver',
     id: 420,
     style: 'cool af'
-}
+};
 
 const {name, ...rest} = cat;
 
-console.log(name, rest)
+console.log(name, rest);
+
+console.log('-----------');
+
+const cat2 = {
+    name: 'Oliver',
+    jumpHeight: 3
+};
+
+const cat2Withid = Object.create(cat2);
+cat2Withid.job = 'security cat'
+
+console.log(cat2);
+console.log(cat2Withid);
+
+cat2.name = 'Maxwell'
+
+console.log(Object.keys(cat2Withid))

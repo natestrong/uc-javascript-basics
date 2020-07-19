2e970c6095e942413bf0103d36941c33e80d227b
Nathan Strong <nathanstrong.fx@gmail.com>
2020-07-19 11:21:09 -0700

    refactor app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,7 +14,36 @@ const num = 99..toFixed();
 console.log(num);
 
 function makeCarPrice() {
-    console.log(arguments)
+    console.log(arguments);
 }
 
-makeCarPrice(1, 2, 3, 45)
+makeCarPrice(1, 2, 3, 45);
+
+const drink = {
+    id: 'xyz',
+    getDrinkDetails() {
+        return `Drink_${this.id}`;
+    },
+    100: 'cool'
+};
+
+console.log(drink.getDrinkDetails());
+
+console.log(drink["100"])
+
+function f(first, ...args) {
+    console.log(first, args)
+}
+
+f('Hi', 'Hello', 'Cool', 'Awesome', 100)
+
+
+const cat = {
+    name: 'Oliver',
+    id: 420,
+    style: 'cool af'
+}
+
+const {name, ...rest} = cat;
+
+console.log(name, rest)
